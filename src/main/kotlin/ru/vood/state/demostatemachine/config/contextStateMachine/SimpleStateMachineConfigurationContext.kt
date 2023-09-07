f4fb016a6b272c2e4d6c0ce9b9dfe9beadcef762
vood <balitsky_igor@list.ru>
2023-09-07 17:14:15 +0300

    init

--- src/main/kotlin/ru/vood/state/demostatemachine/config/contextStateMachine/SimpleStateMachineConfigurationContext.kt
+++ src/main/kotlin/ru/vood/state/demostatemachine/config/contextStateMachine/SimpleStateMachineConfigurationContext.kt
@@ -11,13 +11,12 @@ import org.springframework.statemachine.config.configurers.StateConfigurer
 import ru.vood.state.demostatemachine.config.contextStateMachine.abstraction.AbstractAction
 import ru.vood.state.demostatemachine.config.contextStateMachine.abstraction.ArrowProperty
 import ru.vood.state.demostatemachine.config.contextStateMachine.abstraction.CommonStateMachineApplicationListenerContext
-import ru.vood.state.demostatemachine.config.contextStateMachine.abstraction.ContextData
 
 
 @Configuration
 @EnableStateMachineFactory(name = ["SimpleStateMachineConfigurationContext"])
 class SimpleStateMachineConfigurationContext(
-    private val setActions: Set<AbstractAction<FlowStatesContext, FlowEventContext, ContextData<FlowStatesContext, FlowEventContext>>>
+    private val setActions: Set<AbstractAction<FlowStatesContext, FlowEventContext, *>>
 ) : StateMachineConfigurerAdapter<FlowStatesContext, FlowEventContext>() {
     val LOGGER = LoggerFactory.getLogger(this.javaClass)
     override fun configure(states: StateMachineStateConfigurer<FlowStatesContext, FlowEventContext>) {
@@ -50,7 +49,7 @@ class SimpleStateMachineConfigurationContext(
 
     private fun configureStates(
         stateConfigurer: StateConfigurer<FlowStatesContext, FlowEventContext>,
-        actions: Map<FlowStatesContext, AbstractAction<FlowStatesContext, FlowEventContext, ContextData<FlowStatesContext, FlowEventContext>>>
+        actions: Map<FlowStatesContext, AbstractAction<FlowStatesContext, FlowEventContext, *>>
     ): StateConfigurer<FlowStatesContext, FlowEventContext> {
         if (actions.isNotEmpty()) {
             val stepProps = actions.entries.toList()
--- src/main/kotlin/ru/vood/state/demostatemachine/config/contextStateMachine/abstraction/ArrowProperty.kt
+++ src/main/kotlin/ru/vood/state/demostatemachine/config/contextStateMachine/abstraction/ArrowProperty.kt
@@ -1,8 +1,11 @@
 package ru.vood.state.demostatemachine.config.contextStateMachine.abstraction
 
+import ru.vood.state.demostatemachine.config.contextStateMachine.FlowEventContext
+import ru.vood.state.demostatemachine.config.contextStateMachine.FlowStatesContext
+
 data class ArrowProperty<NODE : Node<NODE, ARROW>, ARROW : Arrow<ARROW, NODE>>(
     val source: NODE,
     val target: NODE,
     val event: ARROW,
-    val action: AbstractAction<NODE, ARROW, ContextData<NODE, ARROW>>
+    val action: AbstractAction<NODE, ARROW, *>
 )
