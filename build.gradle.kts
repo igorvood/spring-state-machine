6248e1f7d54d89b394db369d7e522371da5e2b0b
vood <igor-vood@gmail.com>
2025-09-07 10:58:23 +0300

    new version spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.16"
-    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("org.springframework.boot") version "3.5.5"
+    id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm")
     kotlin("plugin.spring")
+    kotlin("plugin.jpa") version "2.2.10"
 }
 
 group = "ru.vood.state"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-}
-
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
@@ -25,12 +22,15 @@ repositories {
 }
 
 dependencies {
+
+    implementation(platform("org.springframework.statemachine:spring-statemachine-bom:4.0.1"))
+
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.retry:spring-retry")
     implementation("org.springframework:spring-aspects")
-    implementation("net.devh:grpc-server-spring-boot-starter:2.14.0.RELEASE")
-    implementation("net.devh:grpc-client-spring-boot-starter:2.14.0.RELEASE")
-    implementation("org.springframework.statemachine:spring-statemachine-starter:3.2.0")
+//    implementation("net.devh:grpc-server-spring-boot-starter:2.14.0.RELEASE")
+//    implementation("net.devh:grpc-client-spring-boot-starter:2.14.0.RELEASE")
+    implementation("org.springframework.statemachine:spring-statemachine-starter")
 //    implementation("org.springframework.statemachine:spring-statemachine-core:3.2.0.RELEASE")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
@@ -42,10 +42,9 @@ dependencies {
     testImplementation("io.projectreactor:reactor-test")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
     }
 }
 
